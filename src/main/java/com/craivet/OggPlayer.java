d2f1d3f6d04c8f489495bc02eb19e24f4975c1c5
Juan Debenedetti <juandebenedetti94@gmail.com>
2023-01-17 18:04:21 -0300

    Update

--- src/main/java/com/craivet/OggPlayer.java
+++ src/main/java/com/craivet/OggPlayer.java
@@ -6,6 +6,7 @@ import uk.co.caprica.vlcj.player.component.AudioPlayerComponent;
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 
 import javax.swing.JFrame;
+import java.util.Objects;
 
 /**
  * <a href="https://www.tutorialspoint.com/vlcj/vlcj_audio_player.htm#:~:text=Open%20project%20mediaPlayer%20as%20created%20in%20Environment%20Setup%20chapter%20in%20Eclipse.&text=Now%20press%20play%20button%20in,button%20will%20pause%20the%20audio">...</a>.
@@ -26,10 +27,15 @@ public class OggPlayer {
 
 		// mediaPlayerComponent.mediaPlayer().media().play(args[0]);
 
-		System.out.println(OggPlayer.class.getClassLoader().getResourceAsStream("sounds/swing_weapon.ogg").toString());
+		// "\\C:\\Users\\juand\\Documents\\IntelliJ IDEA\\Game2D-3\\target\\classes\\sounds\\swing_weapon.ogg"
 
-		// "C:\\Users\\juand\\Documents\\IntelliJ IDEA\\Game2D-3\\src\\main\\resources\\sounds\\swing_weapon.ogg"
-		// audioPlayerComponent.mediaPlayer().media().startPaused(OggPlayer.class.getClassLoader().getResource("sounds/swing_weapon.ogg").getPath());
+		String path = Objects.requireNonNull(OggPlayer.class.getClassLoader().getResource("sounds/swing_weapon.ogg")).getPath();
+		String formatPath;
+		formatPath = path.replace("%20", " ");
+		String asd = formatPath.replaceAll("/", "\\\\");
+		System.out.println(asd);
+
+		audioPlayerComponent.mediaPlayer().media().startPaused(asd);
 		audioPlayerComponent.mediaPlayer().controls().play();
 	}
 
